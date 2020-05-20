fbed6b917a256e024a72f273d6e0178ecf4d1eb0
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-05-20 16:56:20 -0500

    added read me file

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs")
-const db = require("./db/db.json")
+let db = require("./db/db.json")
 const uuid = require('uuid');
 
 const app = express();
 // lets the port equal whatever heroku chooses for it. 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -14,11 +14,12 @@ app.use(express.json());
 // sets up a public folder access for express to access files in the folder
 app.use(express.static("public"))
 
-// creates a get on the dp.json file
+// returns db file to render saved notes is they exist in db.json
 app.get("/api/notes", function (req, res) {
     return res.json(db);
 });
 
+// returns new note to post request and writes new note to db.json
 app.post("/api/notes", function (req, res) {
     let dbList = db;
     let newNote = {
@@ -34,8 +35,9 @@ app.post("/api/notes", function (req, res) {
         }
         res.json(newNote)
     })
-})
+});
 
+// filters db.son by selected item id and removes it from the array. Then writes new array to db.json
 app.delete("/api/notes/:id", function (req, res) {
     let deleteId = req.params.id;
     let dbList = db.filter(item => item.id !== deleteId)
@@ -44,12 +46,12 @@ app.delete("/api/notes/:id", function (req, res) {
         if (err) {
             console.log(err)
         }
-        res.json(newNote)
+        db = dbList;
+        res.json({});
     })
-
 })
 
-// creates path for notes and default path
+// creates path for notes and default path to home page
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
